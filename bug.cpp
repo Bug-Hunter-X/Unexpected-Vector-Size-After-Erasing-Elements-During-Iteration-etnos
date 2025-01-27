edb3b9f329e8961aa8a1ed24267cf6b528d88d15
BugHunter-X <askzenai@gmail.com>
2025-01-27 10:34:07 +0100

    Add bug.cpp file

---
+++ bug.cpp
@@ -0,0 +1,9 @@
+std::vector<int> vec; 
+for (int i = 0; i < 10; ++i) { 
+  vec.push_back(i); 
+}
+for (int i = 0; i < vec.size(); ++i) { 
+  vec.erase(vec.begin() + i); 
+}
+// Expected size of vec: 0
+// Actual size of vec: 5
